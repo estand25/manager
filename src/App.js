bee0e70cf0e27a943bc19516ca3cacd5b512bd08
Standley Eugene <estand25@gmail.com>
2018-03-11 15:08:40 -0400

    Section 14, Lecture 104 - Just a Touch More Set-up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common';
+
+const App = () => (
+  <View>
+    <Header headerText="Manager" />
+  </View>
+);
+
+export default App;
