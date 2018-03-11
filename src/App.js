2306dda9087b2dec9ab27a9b5d6bdb9777c3ce61
Standley Eugene <estand25@gmail.com>
2018-03-11 19:03:28 -0400

    Section 14, Lecture 108 - More on Boilerplate Set-up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAHWKmdHEz0bVZmuIxr0Qq87uhOb7gNnIs',
+      authDomain: 'manager-16bbd.firebaseapp.com',
+      databaseURL: 'https://manager-16bbd.firebaseio.com',
+      projectId: 'manager-16bbd',
+      storageBucket: 'manager-16bbd.appspot.com',
+      messagingSenderId: '940413259952'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      // <Provider store={createStore()}>
+       <Provider store={createStore(reducers)}>
         <View>
           <Header headerText="Manager" />
           <Text>Hello!</Text>
         </View>
-      // </Provider>
+       </Provider>
     );
   }
 }
