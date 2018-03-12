e188b34f285acccc49c6acbac32d6b7d88313c53
Standley Eugene <estand25@gmail.com>
2018-03-12 19:36:10 -0400

    Section 16, Lecture 114 - Immutable State

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      console.log('Light, Camera, Action!'); 
+      
     default:
       return state;
   }
