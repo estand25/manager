fdbe2228705102aca433ed086ced8f15fd5eeb70
Standley Eugene <estand25@gmail.com>
2018-03-15 20:01:37 -0400

    Section 16, Lecture 122 - Creating User Accounts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => ({
@@ -17,8 +18,21 @@ export const passwordChanged = (text) => ({
 
 export const loginUser = ({ email, password }) => (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS,
-          payload: user });
-      });
+      .then(user => logInUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => logInUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+    });
+  };
+
+  const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL })
+  };
+
+  const logInUserSuccess = (dispatch, user) => {
+    dispatch({
+      type: LOGIN_USER_SUCCESS,
+      payload: user
+    });
   };
