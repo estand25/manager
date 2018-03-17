5da996998a8c3ee7ad1d658580887e738a459fa4
Standley Eugene <estand25@gmail.com>
2018-03-17 16:51:45 -0400

    Section 17, Lecture 135 - Handling Form Updates at Reducer Level

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,13 +1,17 @@
-import { EMPLOYEE_UPDATE } from '../actions/types'
+import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
-export default (state = INITIAL_STATE, action ) => {
-  switch (expression) {
-    case expression:
-
-      break;
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { props: 'name', value: 'jane' }
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
-
+      return state;
   }
 };
