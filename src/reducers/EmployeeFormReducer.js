578c741649d2beb14116f3c834d7ceb3a41f71b9
Standley Eugene <estand25@gmail.com>
2018-03-17 16:43:29 -0400

    Section 17, Lecture 134 - Employee Form Actions

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,13 @@
+import { EMPLOYEE_UPDATE } from '../actions/types'
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action ) => {
+  switch (expression) {
+    case expression:
+
+      break;
+    default:
+
+  }
+};
