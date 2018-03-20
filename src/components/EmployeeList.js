ec04fd29c927739ad9334a6321dec56fd83406e5
Standley Eugene <estand25@gmail.com>
2018-03-20 18:19:06 -0400

    Section 18, 148 - Transforming Object to Arrays

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListView, View, Text, FlatList } from 'react-native';
@@ -6,30 +7,29 @@ import { employeesFetch } from '../actions';
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
-
-    this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
-    // nextProps are the next set of props that this component
-    // will be rendered with
-    // this.props is still the old set of props
-
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(employees);
+  renderRow({ item }) {
+    console.log(item.name);
+    return <Text> {item.name} </Text>;
   }
 
   render() {
+    console.log(this.props);
+    return (
+      <FlatList
+        data={this.props.employees}
+        renderItem={this.renderRow}
+        keyExtractor={(item) => item.uid}
+      />
+    );
   }
 }
 
-const mapStateToProps = (state) => ({ employees: state.employees });
+const mapStateToProps = (state) => {
+  const employees = _.map(state.employees, (val, uid) => ({ ...val, uid }));
+
+  return { employees };
+};
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -3,9 +3,9 @@ import {
 } from '../actions/types';
 
 const INITAL_STATE = {
-  name: '',
-  phone: '',
-  shift: ''
+  // name: '',
+  // phone: '',
+  // shift: ''
 };
 
 export default (state = INITAL_STATE, action) => {
