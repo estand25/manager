8af67016dab93daa82494a1088caeb0ed822aece
Standley Eugene <estand25@gmail.com>
2018-03-20 17:35:18 -0400

    Section 18, 147 - Dynamic DataSource Building

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, FlatList } from 'react-native';
+import { ListView, View, Text, FlatList } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
-  // componentWillMount() {
-  //   this.props.employeesFetch();
-  // }
+  componentWillMount() {
+    this.props.employeesFetch();
 
-  renderRow({ item }) {
-    const { name, phone, shift } = item;
+    this.createDataSource(this.props);
+  }
 
-    return (
-      <View>
-        <Text>{name}</Text>
-        <Text>{phone}</Text>
-        <Text>{shift}</Text>
-      </View>
-    );
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
-    console.log(this.props.employees);
-    return (
-      // <Text>'dd'</Text>
-      <FlatList
-        data={this.props.employees}
-        renderItem={this.renderRow}
-        keyExtractor={(employee) => employee.item}
-      />
-    );
   }
 }
 
