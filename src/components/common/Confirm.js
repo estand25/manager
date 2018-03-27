2ac368737f26925e298a34b0de099dac4d3c704a
Standley Eugene <estand25@gmail.com>
2018-03-27 19:55:01 -0400

    Section 19, Lecture 159 - The Modal Component Continued

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Text, View, Modal } from 'react-native';
-import { CardSection} from './CardSection';
+import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {
-
-};
+const Confirm = ({ childern, visible, onAccept, onDecline }) => (
+    <Modal
+      visible={visible}
+      transparent
+      animationType="slide"
+      onRequestClose={() => {}}
+    >
+      <View>
+        <CardSection>
+          <Text>{childern}</Text>
+        </CardSection>
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
 
 export { Confirm };
