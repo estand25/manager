dbb3dc7ff2cd78e690f45ef45503767e74e34cf9
Standley Eugene <estand25@gmail.com>
2018-03-27 20:13:25 -0400

    Section 19, Lecture 160 - Styling the Modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { employeeUpdate, employeeSave, employeeClear } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  state = {showModal: false};
+
   componentWillMount() {
     _.each(this.props.employee, (value, props) => {
       this.props.employeeUpdate({ props, value });
@@ -37,6 +39,10 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  changeModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
   render() {
     return (
       <Card>
@@ -46,11 +52,24 @@ class EmployeeEdit extends Component {
             Save Changes
           </Button>
         </CardSection>
+
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>
             Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={this.changeModal.bind(this)}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you Sure you want to delete this?
+        </Confirm>
       </Card>
     );
   }
--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,23 +3,45 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = ({ childern, visible, onAccept, onDecline }) => (
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, cardSectionStyle, textStyle } = styles;
+
+  return (
     <Modal
       visible={visible}
       transparent
       animationType="slide"
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{childern}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>
+            {children}
+          </Text>
         </CardSection>
         <CardSection>
           <Button onPress={onAccept}>Yes</Button>
           <Button onPress={onDecline}>No</Button>
         </CardSection>
       </View>
-    </Modal>
-  );
+    </Modal>);
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+};
 
 export { Confirm };
